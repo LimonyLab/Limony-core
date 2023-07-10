07656fd400837299fe3fe558659b52ccfe7a61b9
Mojtaba Eshghie <eshghie@kth.se>
2023-07-10 16:23:35 +0200

    #11 resolved

--- ui/src/components/Chat/SupervisorChatBox.js
+++ ui/src/components/Chat/SupervisorChatBox.js
@@ -1,12 +1,12 @@
 // src/components/Chat/SupervisorChatBox.js
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState, useContext, useRef } from 'react'; // Import useRef
 import MyMessage from './MyMessage';
 import SupervisorMessage from './SupervisorMessage';
 import SendMessageForm from './SendMessageForm';
 import axios from 'axios';
-import { AuthProvider, AuthContext } from '../../context/auth'; // Added AuthContext
-import { useAuth } from '../../context/auth'; // Corrected path to useAuth
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import { AuthProvider, AuthContext } from '../../context/auth';
+import { useAuth } from '../../context/auth';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const ChatContainer = styled.div`
@@ -28,18 +28,21 @@ function SupervisorChatBox({ conversationId }) {
     const [messages, setMessages] = useState([]);
     const { authToken } = useContext(AuthContext);
 
-    const navigate = useNavigate(); // Initialize useNavigate
+    const navigate = useNavigate();
     const { currentUser } = useAuth();
+    
+    const messagesEndRef = useRef(null) // Create a ref
+
+    const scrollToBottom = () => { // Function to scroll to the bottom
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+    }
 
     useEffect(() => {
-    if (!currentUser) {
-        navigate('/login');
-    }
+        if (!currentUser) {
+            navigate('/login');
+        }
     }, [currentUser, navigate]);
 
-
-
-        
     useEffect(() => {
         axios.get(`http://localhost:3000/chat/get-messages/${conversationId}`, {
             headers: {
@@ -47,21 +50,21 @@ function SupervisorChatBox({ conversationId }) {
             }
             })
             .then(response => {
-                //console.log('#### The received messages are: ', response.data.conversation)
                 setMessages(response.data.conversation);
             })
             .catch(error => {
                 console.error('Error fetching messages: ', error);
             });
     }, []);
-    
+
+    useEffect(scrollToBottom, [messages]); // Call scrollToBottom each time messages updates
 
     const handleSend = (messageContent) => {
         console.log('Our authorization bearer token is: ', authToken);
         axios.post(`http://localhost:3000/chat/new-message/${conversationId}`, 
         {
             content: messageContent,
-            sender: currentUser.email, // added sender field
+            sender: currentUser.email,
         },
         {
             headers: {
@@ -69,11 +72,10 @@ function SupervisorChatBox({ conversationId }) {
             }
         })
         .then(response => {
-            // You could optimistically add the new message to the UI here
             const newMessage = {
               content: messageContent,
               sender: currentUser.email,
-              createdAt: new Date(), // added timestamp
+              createdAt: new Date(),
             };
             setMessages([...messages, newMessage]);
         })
@@ -92,6 +94,7 @@ function SupervisorChatBox({ conversationId }) {
                     ? <SupervisorMessage key={index} {...message} />
                     : <MyMessage key={index} {...message} />
                 )}
+                <div ref={messagesEndRef} /> {/* Add a ref to an empty div at the end of messages */}
             </MessagesContainer>
             <SendMessageForm onSend={handleSend} />
       </ChatContainer>
