2d67fec9650ca9b00ec6b0d67c1c8d6ede425758
Mojtaba Eshghie <eshghie@kth.se>
2023-07-12 11:42:21 +0200

    #13 trying to resolve this issue

--- user-management-service/src/middleware/auth.js
+++ user-management-service/src/middleware/auth.js
@@ -6,6 +6,16 @@ const logger = require('../utils/logger');
 const SECRET_KEY = '123456789';
 
 
+/**
+ * TODO: Role-based access control: Let's create add a functionality to the auth middleware so that there are multiple roles defined in
+ * a certain mapping type. Each role in this file has access to a list of paths (type:string). Likewise, another practical mapping will
+ * be created based on the previous mapping that maps paths to roles able to access them. Therefore, auth middleware checks each request path 
+ * against the latter mapping to see which roles can access this path. If the role has access to this path there are two different 
+ * possible outcomes: 1. Both the path and the actual resource are accessible by the exact user (like /dashboard/, that's accessible
+ * by every logged-in user) 2. The role has access to the path, but, the exact user is not authorized to access to that exact resource (user 1 cannot
+ *  access user 2's chat page via /chat/conversationId; or, 'employee' role-holding users cannot access /chat/supervisor-panel, ).     
+ */
+
 const auth = async (req, res, next) => {
   //console.log('The request is: ', req);
   const authHeader = req.header('Authorization');
