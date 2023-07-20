6fcf7cddc2108f519708694153eacf22bd920173
Mojtaba Eshghie <eshghie@kth.se>
2023-07-20 10:27:10 +0200

    commiting uncompilable changes due to my laptop being stuck...

--- user-management-service/src/routes/userRoutes.js
+++ user-management-service/src/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const userController = require('../controllers/userController');
-const auth = require('../middleware/auth');
+const {auth} = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
-router.get('/profile', auth, userController.getProfile); // protected route
+//router.get('/profile', auth, userController.getProfile); // protected route
+router.get('/profile', auth, userController.test); // protected route
 router.put('/profile', auth, userController.updateProfile); // protected route
 router.delete('/profile', auth, userController.deleteProfile); // protected route
 
